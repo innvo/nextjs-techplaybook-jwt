ba6f3bcd39b89fe56ad013b3c82a0383a6281cc0
echasin <echasin@innvosolutions.com>
2023-04-26 14:29:43 -0700

    WIP -Added Dialog Header

--- src/content/techplaybook/project/NewProjectDialog.tsx
+++ src/content/techplaybook/project/NewProjectDialog.tsx
@@ -1,3 +1,6 @@
+import { useState } from 'react';
+import { useTranslation } from 'react-i18next';
+
 import {
     Button,
     Dialog,
@@ -6,8 +9,9 @@ import {
     DialogContentText,
     DialogTitle,
     TextField,
+    Typography
   } from '@mui/material';
-  import { useState } from 'react';
+
 //   import api from '../services/api';
   
   interface NewProjectDialogProps {
@@ -17,6 +21,7 @@ import {
   
   function NewProjectDialog({ open, onClose }: NewProjectDialogProps) {
     console.log('In src/content/techplaybook/project/NewProjectDialog.tsx')
+    const { t }: { t: any } = useTranslation();
     const [name, setName] = useState('');
     const [description, setDescription] = useState('');
   
@@ -37,34 +42,424 @@ import {
     };
   
     return (
-      <Dialog open={open} onClose={onClose}>
-        <DialogTitle>Create New Project</DialogTitle>
-        <DialogContent>
-          <DialogContentText>Please enter project name and description</DialogContentText>
-          <TextField
-            autoFocus
-            margin="dense"
-            id="name"
-            label="Project Name"
-            type="text"
-            fullWidth
-            value={name}
-            onChange={(e) => setName(e.target.value)}
-          />
-          <TextField
-            margin="dense"
-            id="description"
-            label="Project Description"
-            type="text"
-            fullWidth
-            value={description}
-            onChange={(e) => setDescription(e.target.value)}
-          />
-        </DialogContent>
-        <DialogActions>
-          <Button onClick={onClose}>Cancel</Button>
-          <Button onClick={handleCreate}>Create</Button>
-        </DialogActions>
+      // <Dialog open={open} onClose={onClose}>
+        <Dialog
+        fullWidth
+        maxWidth="md"
+        open={open}
+        onClose={onClose}
+      >
+        <DialogTitle
+          sx={{
+            p: 3
+          }}
+        >
+          <Typography variant="h4" gutterBottom>
+            {t('Create new project')}
+          </Typography>
+          <Typography variant="subtitle2">
+            {t('Use this dialog window to add a new project')}
+          </Typography>
+        </DialogTitle> 
+        {/* <Formik
+          initialValues={{
+            title: '',
+            submit: null
+          }}
+          validationSchema={Yup.object().shape({
+            title: Yup.string()
+              .max(255)
+              .required(t('The title field is required'))
+          })}
+          onSubmit={async (
+            _values,
+            { resetForm, setErrors, setStatus, setSubmitting }
+          ) => {
+            try {
+              await wait(1000);
+              resetForm();
+              setStatus({ success: true });
+              setSubmitting(false);
+              handleCreateProjectSuccess();
+            } catch (err) {
+              console.error(err);
+              setStatus({ success: false });
+              setErrors({ submit: err.message });
+              setSubmitting(false);
+            }
+          }}
+        >
+          {({
+            errors,
+            handleBlur,
+            handleChange,
+            handleSubmit,
+            isSubmitting,
+            touched,
+            values
+          }) => (
+            <form onSubmit={handleSubmit}>
+              <DialogContent
+                dividers
+                sx={{
+                  p: 3
+                }}
+              >
+                <Grid container spacing={0}>
+                  <Grid
+                    item
+                    xs={12}
+                    sm={4}
+                    md={3}
+                    justifyContent="flex-end"
+                    textAlign={{ sm: 'right' }}
+                  >
+                    <Box
+                      pr={3}
+                      sx={{
+                        pt: `${theme.spacing(2)}`,
+                        pb: { xs: 1, md: 0 }
+                      }}
+                      alignSelf="center"
+                    >
+                      <b>{t('Project title')}:</b>
+                    </Box>
+                  </Grid>
+                  <Grid
+                    sx={{
+                      mb: `${theme.spacing(3)}`
+                    }}
+                    item
+                    xs={12}
+                    sm={8}
+                    md={9}
+                  >
+                    <TextField
+                      error={Boolean(touched.title && errors.title)}
+                      fullWidth
+                      helperText={touched.title && errors.title}
+                      name="title"
+                      placeholder={t('Project title here...')}
+                      onBlur={handleBlur}
+                      onChange={handleChange}
+                      value={values.title}
+                      variant="outlined"
+                    />
+                  </Grid>
+                  <Grid item xs={12} sm={4} md={3} textAlign={{ sm: 'right' }}>
+                    <Box
+                      pr={3}
+                      sx={{
+                        pb: { xs: 1, md: 0 }
+                      }}
+                    >
+                      <b>{t('Description')}:</b>
+                    </Box>
+                  </Grid>
+                  <Grid
+                    sx={{
+                      mb: `${theme.spacing(3)}`
+                    }}
+                    item
+                    xs={12}
+                    sm={8}
+                    md={9}
+                  >
+                    <EditorWrapper>
+                      <ReactQuill />
+                    </EditorWrapper>
+                  </Grid>
+                  <Grid
+                    item
+                    xs={12}
+                    sm={4}
+                    md={3}
+                    justifyContent="flex-end"
+                    textAlign={{ sm: 'right' }}
+                  >
+                    <Box
+                      pr={3}
+                      sx={{
+                        pt: `${theme.spacing(2)}`,
+                        pb: { xs: 1, md: 0 }
+                      }}
+                      alignSelf="center"
+                    >
+                      <b>{t('Tags')}:</b>
+                    </Box>
+                  </Grid>
+                  <Grid
+                    sx={{
+                      mb: `${theme.spacing(3)}`
+                    }}
+                    item
+                    xs={12}
+                    sm={8}
+                    md={9}
+                  >
+                    <Autocomplete
+                      multiple
+                      sx={{
+                        m: 0
+                      }}
+                      limitTags={2}
+                      // @ts-ignore
+                      getOptionLabel={(option) => option.title}
+                      options={projectTags}
+                      renderInput={(params) => (
+                        <TextField
+                          {...params}
+                          fullWidth
+                          variant="outlined"
+                          placeholder={t('Select project tags...')}
+                        />
+                      )}
+                    />
+                  </Grid>
+                  <Grid item xs={12} sm={4} md={3} textAlign={{ sm: 'right' }}>
+                    <Box
+                      pr={3}
+                      sx={{
+                        pb: { xs: 1, md: 0 }
+                      }}
+                    >
+                      <b>{t('Upload files')}:</b>
+                    </Box>
+                  </Grid>
+                  <Grid
+                    sx={{
+                      mb: `${theme.spacing(3)}`
+                    }}
+                    item
+                    xs={12}
+                    sm={8}
+                    md={9}
+                  >
+                    <BoxUploadWrapper {...getRootProps()}>
+                      <input {...getInputProps()} />
+                      {isDragAccept && (
+                        <>
+                          <AvatarSuccess variant="rounded">
+                            <CheckTwoToneIcon />
+                          </AvatarSuccess>
+                          <Typography
+                            sx={{
+                              mt: 2
+                            }}
+                          >
+                            {t('Drop the files to start uploading')}
+                          </Typography>
+                        </>
+                      )}
+                      {isDragReject && (
+                        <>
+                          <AvatarDanger variant="rounded">
+                            <CloseTwoToneIcon />
+                          </AvatarDanger>
+                          <Typography
+                            sx={{
+                              mt: 2
+                            }}
+                          >
+                            {t('You cannot upload these file types')}
+                          </Typography>
+                        </>
+                      )}
+                      {!isDragActive && (
+                        <>
+                          <AvatarWrapper variant="rounded">
+                            <CloudUploadTwoToneIcon />
+                          </AvatarWrapper>
+                          <Typography
+                            sx={{
+                              mt: 2
+                            }}
+                          >
+                            {t('Drag & drop files here')}
+                          </Typography>
+                        </>
+                      )}
+                    </BoxUploadWrapper>
+                    {files.length > 0 && (
+                      <>
+                        <Alert
+                          sx={{
+                            py: 0,
+                            mt: 2
+                          }}
+                          severity="success"
+                        >
+                          {t('You have uploaded')} <b>{files.length}</b>{' '}
+                          {t('files')}!
+                        </Alert>
+                        <Divider
+                          sx={{
+                            mt: 2
+                          }}
+                        />
+                        <List disablePadding component="div">
+                          {files}
+                        </List>
+                      </>
+                    )}
+                  </Grid>
+                  <Grid
+                    item
+                    xs={12}
+                    sm={4}
+                    md={3}
+                    justifyContent="flex-end"
+                    textAlign={{ sm: 'right' }}
+                  >
+                    <Box
+                      pr={3}
+                      sx={{
+                        pt: `${theme.spacing(2)}`,
+                        pb: { xs: 1, md: 0 }
+                      }}
+                      alignSelf="center"
+                    >
+                      <b>{t('Members')}:</b>
+                    </Box>
+                  </Grid>
+                  <Grid
+                    sx={{
+                      mb: `${theme.spacing(3)}`
+                    }}
+                    item
+                    xs={12}
+                    sm={8}
+                    md={9}
+                  >
+                    <Autocomplete
+                      multiple
+                      sx={{
+                        m: 0
+                      }}
+                      limitTags={2}
+                      // @ts-ignore
+                      getOptionLabel={(option) => option.title}
+                      options={members}
+                      renderOption={(props, option) => (
+                        <li {...props}>
+                          <Avatar
+                            sx={{
+                              mr: 1
+                            }}
+                            src={option.avatar}
+                          />
+                          {option.name}
+                        </li>
+                      )}
+                      renderInput={(params) => (
+                        <TextField
+                          {...params}
+                          variant="outlined"
+                          fullWidth
+                          InputLabelProps={{
+                            shrink: true
+                          }}
+                          placeholder={t('Select project members...')}
+                        />
+                      )}
+                      renderTags={(members, getTagProps) =>
+                        members.map((ev, index: number) => (
+                          <Chip
+                            key={ev.name}
+                            label={ev.name}
+                            {...getTagProps({ index })}
+                            avatar={<Avatar src={ev.avatar} />}
+                          />
+                        ))
+                      }
+                    />
+                  </Grid>
+                  <Grid
+                    item
+                    xs={12}
+                    sm={4}
+                    md={3}
+                    justifyContent="flex-end"
+                    textAlign={{ sm: 'right' }}
+                  >
+                    <Box
+                      pr={3}
+                      sx={{
+                        pt: `${theme.spacing(2)}`,
+                        pb: { xs: 1, md: 0 }
+                      }}
+                      alignSelf="center"
+                    >
+                      <b>{t('Due Date')}:</b>
+                    </Box>
+                  </Grid>
+                  <Grid
+                    sx={{
+                      mb: `${theme.spacing(3)}`
+                    }}
+                    item
+                    xs={12}
+                    sm={8}
+                    md={9}
+                  >
+                    <DatePicker
+                      value={value}
+                      onChange={(newValue) => {
+                        setValue(newValue);
+                      }}
+                      renderInput={(params) => (
+                        <TextField
+                          placeholder={t('Select due date...')}
+                          {...params}
+                        />
+                      )}
+                    />
+                  </Grid>
+
+                  <Grid
+                    item
+                    xs={12}
+                    sm={4}
+                    md={3}
+                    textAlign={{ sm: 'right' }}
+                  />
+                  <Grid
+                    sx={{
+                      mb: `${theme.spacing(3)}`
+                    }}
+                    item
+                    xs={12}
+                    sm={8}
+                    md={9}
+                  >
+                    <Button
+                      sx={{
+                        mr: 2
+                      }}
+                      type="submit"
+                      startIcon={
+                        isSubmitting ? <CircularProgress size="1rem" /> : null
+                      }
+                      disabled={Boolean(errors.submit) || isSubmitting}
+                      variant="contained"
+                      size="large"
+                    >
+                      {t('Create project')}
+                    </Button>
+                    <Button
+                      color="secondary"
+                      size="large"
+                      variant="outlined"
+                      onClick={handleCreateProjectClose}
+                    >
+                      {t('Cancel')}
+                    </Button>
+                  </Grid>
+                </Grid>
+              </DialogContent>
+            </form>
+          )}
+        </Formik>  */}
       </Dialog>
     );
   }
